343b7ee0e9d8c9b76d64925f7027457f9d48b119
Suhani-soni <suhani898989@gmail.com>
2024-08-03 18:27:50 +0530

    first commit

---
+++ BINARYTREE1.java
@@ -0,0 +1,62 @@
+public class BINARYTREE1 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+             this.right = null;
+        }
+    }
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+        idx++;
+        if(nodes[idx] == -1){
+            return null;
+        }
+        Node newnode = new Node(nodes[idx]);
+        newnode.left = buildTree(nodes);
+        newnode.right = buildTree(nodes);
+        return newnode;
+
+    }
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+
+        }
+        public static void postOrder(Node root){
+            if(root == null){
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+public static void main(String args[]){
+    int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    BinaryTree tree = new BinaryTree();
+    Node root = tree.buildTree(nodes);
+    //System.out.println(root.data);
+  //  tree.preOrder(root);
+  //  tree.inOrder(root);
+    tree.postOrder(root);
+   
+} 
+}
---
+++ BINARYTREE6.java
@@ -0,0 +1,3 @@
+public class BINARYTREE6 {
+    
+}
